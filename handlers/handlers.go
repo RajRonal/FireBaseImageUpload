70a4afe7a760e045511ed19914368106b3f51df0
RajRonal <ronal.raj@remotestate.com>
2022-08-03 14:55:48 +0530

    Add existing project files to Git

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,72 +1,78 @@
 package handlers
 
 import (
-	"FireBaseEx/helper"
 	"FireBaseEx/models"
 	cloud "cloud.google.com/go/storage"
 	"context"
 	"encoding/json"
 	firebase "firebase.google.com/go"
+	"fmt"
 	"google.golang.org/api/option"
 	"io"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 func UploadImage(writer http.ResponseWriter, request *http.Request) {
-	route := models.App{}
+	client := models.App{}
 	var err error
-	//ctx := context.Background()
-	route.Ctx = context.Background()
-	credentialsFile := option.WithCredentialsFile("serviceAccountKey.json")
-	//fmt.Println(credentialsFile)
-	app, err := firebase.NewApp(route.Ctx, nil, credentialsFile)
+	client.Ctx = context.Background()
+	credentialsFile := option.WithCredentialsJSON([]byte(os.Getenv("FIRE_KEY")))
+	fmt.Println(credentialsFile)
+	app, err := firebase.NewApp(client.Ctx, nil, credentialsFile)
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatal(err)
 	}
 
-	route.Client, err = app.Firestore(route.Ctx)
+	client.Client, err = app.Firestore(client.Ctx)
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatal(err)
 	}
 
-	route.Storage, err = cloud.NewClient(route.Ctx, credentialsFile)
+	client.Storage, err = cloud.NewClient(client.Ctx, credentialsFile)
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatal(err)
 	}
 
 	file, fileHeader, err := request.FormFile("image")
 	err = request.ParseMultipartForm(10 << 20)
 	if err != nil {
+		writer.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
 	defer file.Close()
-
 	imagePath := fileHeader.Filename
-
 	bucket := "image-a5e55.appspot.com"
-
-	wc := route.Storage.Bucket(bucket).Object(imagePath).NewWriter(route.Ctx)
-	_, err = io.Copy(wc, file)
+	bucketStorage := client.Storage.Bucket(bucket).Object(imagePath).NewWriter(client.Ctx)
+	_, err = io.Copy(bucketStorage, file)
 	if err != nil {
 		writer.WriteHeader(http.StatusBadRequest)
 		return
-
 	}
-	if err := wc.Close(); err != nil {
+
+	if err := bucketStorage.Close(); err != nil {
 		writer.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	imageUrl := helper.CreateImageUrl(imagePath, bucket)
+
+	signedUrl := &cloud.SignedURLOptions{
+		Scheme:  cloud.SigningSchemeV4,
+		Method:  "GET",
+		Expires: time.Now().Add(15 * time.Minute),
+	}
+	url, err := client.Storage.Bucket(bucket).SignedURL(imagePath, signedUrl)
 	if err != nil {
-		writer.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	errs := json.NewEncoder(writer).Encode(imageUrl)
+
+	log.Println(url)
+	errs := json.NewEncoder(writer).Encode(url)
 	if errs != nil {
 		writer.WriteHeader(http.StatusInternalServerError)
 		return
-
 	}
+
 }
